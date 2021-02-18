99ab21fef2bbc9a7027c426bd4d9274706024d14
Özge Asar <ozasgear@gmail.com>
2021-02-18 22:32:21 +0300

    Final App

---
+++ lib/med_info.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class MedInfo extends StatefulWidget {
+  @override
+  _MedInfoState createState() => _MedInfoState();
+}
+
+class _MedInfoState extends State<MedInfo> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+    );
+  }
+}
